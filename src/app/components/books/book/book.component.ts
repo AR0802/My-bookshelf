95ce9dc4bbfae6c6f38dfe9e31a5401e06186faa
AR0802 <91504021+AR0802@users.noreply.github.com>
2025-04-08 21:59:36 +0300

    feat: add book preview page

--- src/app/components/books/book/book.component.ts
+++ src/app/components/books/book/book.component.ts
@@ -1,9 +1,11 @@
 import { ChangeDetectionStrategy, Component, input } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 import { IBook } from '@shared/book.interface';
 
 @Component({
 	selector: 'app-book',
+	imports: [RouterLink],
 	templateUrl: './book.component.html',
 	styleUrl: './book.component.scss',
 	changeDetection: ChangeDetectionStrategy.OnPush,
