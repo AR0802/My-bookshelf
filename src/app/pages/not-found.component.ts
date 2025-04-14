8c3cadeb589fbe2bc8a13aaaaa957436aa05e9a8
AR0802 <91504021+AR0802@users.noreply.github.com>
2025-04-14 20:44:22 +0300

    feat: add themes

--- src/app/pages/not-found.component.ts
+++ src/app/pages/not-found.component.ts
@@ -3,6 +3,6 @@ import { Component } from '@angular/core';
 @Component({
 	selector: 'app-not-found',
 	template: '<h1>Page Not Found!</h1>',
-	styles: 'h1 {text-align: center; color: #fff}',
+	styles: 'h1 {text-align: center; color: var(--card-bg-color)}',
 })
 export default class NotFoundComponent {}
